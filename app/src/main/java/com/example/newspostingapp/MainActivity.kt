26e02e1d906b5875d18c574758c5a42edefc8d9c
rishi0218 <sagarbonthu12@gmail.com>
2025-03-20 23:46:00 +0530

    Designed Splash screen and sign-up pages

---
+++ app/src/main/java/com/example/newspostingapp/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.example.newspostingapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.newspostingapp.ui.theme.NewsPostingAppTheme
+import kotlinx.coroutines.delay
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            StartUpViewScreen(::navigationLogic)
+        }
+    }
+
+    private fun navigationLogic(navigationValue: Int) {
+
+        when (navigationValue) {
+            1 -> {
+                startActivity(Intent(this, SignInActivity::class.java))
+                finish()
+            }
+
+        }
+    }
+}
+
+@Composable
+fun StartUpViewScreen(navigationLogic: (navigationValue: Int) -> Unit) {
+    var canSplash by remember { mutableStateOf(true) }
+
+    LaunchedEffect(Unit) {
+        delay(3000)
+        canSplash = false
+    }
+
+    if (canSplash) {
+        StartUpView()
+    } else {
+        navigationLogic.invoke(1)
+    }
+}
+
+@Composable
+fun StartUpView() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Green),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+            {
+
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(color = Color.Transparent),
+                )
+                {
+                    Image(
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        painter = painterResource(id = R.drawable.news_posting),
+                        contentDescription = "News Posting App",
+                    )
+
+                    Spacer(modifier = Modifier.height(6.dp))
+
+                    Text(
+                        text = "News Posting App\nby Rishi",
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF64A70B), // Green color similar to the design
+                        fontSize = 26.sp,
+                        style = MaterialTheme.typography.headlineLarge.copy(fontWeight = FontWeight.Bold),
+                        modifier = Modifier.align(Alignment.CenterHorizontally),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            }
+
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/newspostingapp/SignUpActivity.kt
@@ -0,0 +1,201 @@
+package com.example.newspostingapp
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SignUpActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RegisterScreen()
+        }
+    }
+}
+
+@Composable
+fun RegisterScreen() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    var fullName by remember { mutableStateOf("") }
+    var qualification by remember { mutableStateOf("") }
+
+    val context = LocalContext.current as Activity
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(0.dp)
+            .background(Color.Green),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    )
+    {
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        )
+        {
+            Spacer(modifier = Modifier.weight(1f))
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+            )
+
+
+            { // Logo (replace with acorn image resource if you have one)
+                Image(
+                    painter = painterResource(id = R.drawable.news_posting), // Replace with your drawable resource
+                    contentDescription = "App Logo",
+                    modifier = Modifier
+                        .size(64.dp)
+                        .align(Alignment.CenterHorizontally)
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Login Title
+                Text(
+                    text = "Register To\nNews Posting",
+                    color = Color(0xFF64A70B), // Green color similar to the design
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    textAlign = TextAlign.Center
+                )
+
+
+                Spacer(modifier = Modifier.height(16.dp))
+                OutlinedTextField(
+                    value = fullName,
+                    onValueChange = { fullName = it },
+                    label = { Text("Full Name") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp)
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+
+
+                // Email TextField
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text("Email Address") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp)
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                // Password TextField
+                OutlinedTextField(
+                    value = qualification, onValueChange = { qualification = it },
+                    label = { Text(text = "Qualification") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(
+                            horizontal = 12.dp,
+                        )
+                )
+                OutlinedTextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("Password") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp)
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+                OutlinedTextField(
+                    value = confirmPassword,
+                    onValueChange = { confirmPassword = it },
+                    label = { Text("Confirm Password") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp)
+                )
+                Spacer(modifier = Modifier.height(14.dp))
+
+
+                // Login Button
+                Button(
+                    onClick = { /* Handle login */ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp)
+                        .height(38.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
+
+                ) {
+                    Text(text = "Register")
+                }
+
+                Spacer(modifier = Modifier.height(15.dp))
+
+                // Forgot Password Text
+                Text(
+                    text = "Login",
+                    color = Color.Red,
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.bodyLarge.copy(fontWeight = FontWeight.Bold),
+                    modifier = Modifier
+                        .clickable {
+                            context.startActivity(Intent(context, SignInActivity::class.java))
+                            context.finish()
+                        }
+                        .align(Alignment.CenterHorizontally)
+
+
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+            }
+            Spacer(modifier = Modifier.weight(1f))
+
+        }
+    }
+}
